713f4e88bba6f48dca33e9d11b381a285516cf88
Mouad LOUHIBI <m.louhibi@giantlink.ma>
2021-05-06 14:29:57 +0000

    Bootstrap 4

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,161 @@
 <template>
-  <div class="row no-gutters">
-    <div class="col-md-6 no-gutters">
-      <div class="leftside d-flex justify-content-center align-items-center"></div>
+  <div class="row mt-5">
+    <div class="col-md-5 mt-5 ml-2 mr-2">
+      <div class="d-flex justify-content-center">
+        <div class="container mt-5 ml-5 mr-5" id="descript">
+          <h3 id="title">Description du poste</h3>
+          <p id="paragraph">
+            tant qu’employeur, la gestion quotidienne de vos salariés nécessite
+            une attention constante, car le personnel de votre société est
+            essentiel à la bonne marche de votre entreprise.
+          </p>
+          <p id="paragraph">
+            Lorsque vous envisagez d’embaucher un nouveau salarié ou de recruter
+            un stagiaire. tant qu’employeur, la gestion quotidienne de vos
+            salariés nécessite une attention constante, car le personnel de
+            votre société est essentiel à la bonne marche de votre entreprise.
+          </p>
+        </div>
+      </div>
     </div>
-    <div class="col-md-6 no-gutters">
-      <div class="rightside d-flex justify-content-center align-items-center"></div>
+    <div class="col-md-6 mt-4 ml-4" id="icons">
+      <div class="d-flex justify-content-center align-items-center">
+        <div class="row">
+          <div class="col-md-12 mb-4" id="div-btn">
+            <button type="button" class="btn btn-outline-primary icons-button">
+              <i class="fas fa-trash"></i>
+            </button>
+            <button type="button" class="btn btn-outline-primary icons-button">
+              <i class="fas fa-trash"></i>
+            </button>
+            <button type="button icons-button" class="btn btn-outline-primary icons-button">
+              <i class="fas fa-trash"></i>
+            </button>
+          </div>
+          <div class="row">
+            <div class="d-flex justify-content-center">
+              <div class="col-md-6 mb-4 no-gutters">
+                <div class="mb-3">
+                  <label for="disabledTextInput" class="form-label"
+                    >Disabled input</label
+                  >
+                  <input
+                    type="text"
+                    id="disabledTextInput"
+                    class="form-control"
+                    placeholder="Disabled input"
+                  />
+                  <label for="disabledTextInput" class="form-label"
+                    >Disabled input</label
+                  >
+                  <input
+                    type="text"
+                    id="disabledTextInput"
+                    class="form-control"
+                    placeholder="Disabled input"
+                  />
+                  <label for="disabledTextInput" class="form-label"
+                    >Disabled input</label
+                  >
+                  <input
+                    type="text"
+                    id="disabledTextInput"
+                    class="form-control"
+                    placeholder="Disabled input"
+                  />
+                  <label for="disabledTextInput" class="form-label"
+                    >Disabled input</label
+                  >
+                  <input
+                    type="text"
+                    id="disabledTextInput"
+                    class="form-control"
+                    placeholder="Disabled input"
+                  />
+                  <label for="disabledTextInput" class="form-label"
+                    >Disabled input</label
+                  >
+                  <input
+                    type="text"
+                    id="disabledTextInput"
+                    class="form-control"
+                    placeholder="Disabled input"
+                  />
+                </div>
+                <div class="mb-3">
+                  <label for="disabledSelect" class="form-label"
+                    >Disabled select menu</label
+                  >
+                  <select id="disabledSelect" class="form-select">
+                    <option>Disabled select</option>
+                  </select>
+                </div>
+              </div>
+              <div class="col-md-6 no-gutters">
+                <div class="mb-3">
+                  <label for="disabledTextInput" class="form-label"
+                    >Disabled input</label
+                  >
+                  <input
+                    type="text"
+                    id="disabledTextInput"
+                    class="form-control"
+                    placeholder="Disabled input"
+                  />
+                  <label for="disabledTextInput" class="form-label"
+                    >Disabled input</label
+                  >
+                  <input
+                    type="text"
+                    id="disabledTextInput"
+                    class="form-control"
+                    placeholder="Disabled input"
+                  />
+                  <label for="disabledTextInput" class="form-label"
+                    >Disabled input</label
+                  >
+                  <input
+                    type="text"
+                    id="disabledTextInput"
+                    class="form-control"
+                    placeholder="Disabled input"
+                  />
+                  <label for="disabledTextInput" class="form-label"
+                    >Disabled input</label
+                  >
+                  <input
+                    type="text"
+                    id="disabledTextInput"
+                    class="form-control"
+                    placeholder="Disabled input"
+                  />
+                  <label for="disabledTextInput" class="form-label"
+                    >Disabled input</label
+                  >
+                  <input
+                    type="text"
+                    id="disabledTextInput"
+                    class="form-control"
+                    placeholder="Disabled input"
+                  />
+                </div>
+                <div class="mb-3">
+                  <label for="disabledSelect" class="form-label"
+                    >Disabled select menu</label
+                  >
+                  <select id="disabledSelect" class="form-select">
+                    <option>Disabled select</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-12" id="div-btn">
+            <button type="button" class="btn click-button">Enregistrer</button>
+            <button type="button" class="btn click-button">Publier</button>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -15,23 +166,20 @@ export default {
 </script>
 
 <style>
-.leftside,
-.rightside {
-  height: 50vh;
-  width: 100%;
+.icons-button {
+  width: 50px;
+  height: 50px;
+  float: right;
+  margin: 0px 1px;
 }
 
-@media screen and (min-width: 768px) {
-  .leftside, .rightside{
-    height: 100vh;
-  }
-}
-
-.leftside {
-  background: red;
-}
-
-.rightside {
-  background: blue;
+.click-button {
+  width: 150px;
+  height: 30px;
+  margin: 0px 5px;
+  float: right;
+  background-color: rgb(30, 30, 226);
+  color: white;
+  padding-top: 2px;
 }
 </style>
